bfe21f458a773836f40c32e7200e137dd446ff88
ababushkin6 <54685325+ababushkin6@users.noreply.github.com>
2019-12-13 17:50:38 -0500

    Add files via upload

--- Sensors/UltrasonicCounter_to_Pi.ino
+++ Sensors/UltrasonicCounter_to_Pi.ino
@@ -1,14 +1,3 @@
-#include "DHT.h"
-
-#define DHTPIN 3     // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-//
-//DHT dht(DHTPIN, DHTTYPE);
-
 const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
 const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
 
@@ -237,40 +226,3 @@ void get_batsignal(int trigPin, int echoPin)
    pinMode(echoPin, INPUT);
    duration = pulseIn(echoPin, HIGH);
 }
-
-//void getDHT()
-//{
-//  // Wait a few seconds between measurements.
-//  delay(1000);
-//
-//  // Reading temperature or humidity takes about 250 milliseconds!
-//  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-//  float h = dht.readHumidity();
-//  // Read temperature as Celsius (the default)
-//  float t = dht.readTemperature();
-//  // Read temperature as Fahrenheit (isFahrenheit = true)
-//  float f = dht.readTemperature(true);
-//
-//  // Check if any reads failed and exit early (to try again).
-//  if (isnan(h) || isnan(t) || isnan(f)) {
-//    Serial.println(F("Failed to read from DHT sensor!"));
-//    return;
-//  }
-//
-//  // Compute heat index in Fahrenheit (the default)
-//  float hif = dht.computeHeatIndex(f, h);
-//  // Compute heat index in Celsius (isFahreheit = false)
-//  float hic = dht.computeHeatIndex(t, h, false);
-//
-//  Serial.print(F("Humidity: "));
-//  Serial.print(h);
-//  Serial.print(F("%  Temperature: "));
-//  Serial.print(t);
-//  Serial.print(F("°C "));
-//  Serial.print(f);
-//  Serial.print(F("°F  Heat index: "));
-//  Serial.print(hic);
-//  Serial.print(F("°C "));
-//  Serial.print(hif);
-//  Serial.println(F("°F"));
-//}
